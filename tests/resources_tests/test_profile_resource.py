df7112e658102439eca295b52ccd7d918703af97
VonCrab <lukepensiero@gmail.com>
2016-06-14 10:34:46 -0400

    profile reasource

--- tests/resources_tests/test_profile_resource.py
+++ tests/resources_tests/test_profile_resource.py
@@ -7,6 +7,7 @@ from ..test_base import AuthorizedTwitterAPITestCase
 class ProfileResourceTestCase(AuthorizedTwitterAPITestCase):
 
     def test_get_profile_successful(self):
+        self.maxDiff = None
         response = self.client.get('/profile/testuser1')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.content_type, 'application/json')
@@ -19,13 +20,13 @@ class ProfileResourceTestCase(AuthorizedTwitterAPITestCase):
             'birth_date': '2016-01-30',
             'tweet': [
                 {
-                    'date': '2016-06-01T05:13:00',
+                    'date': '2016-06-01 05:13:00',
                     'id': 1,
                     'text': 'Tweet 1 testuser1',
                     'uri': '/tweet/1'
                 },
                 {
-                    'date': '2016-06-01T05:22:00',
+                    'date': '2016-06-01 05:22:00',
                     'id': 2,
                     'text': 'Tweet 2 testuser1',
                     'uri': '/tweet/2'
--- twitter_api/main.py
+++ twitter_api/main.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 import binascii
 import sqlite3
 import os
@@ -13,14 +11,20 @@ from utils import *
 app = Flask(__name__)
 api = Api(app)
 
+profile_properties = ['user_id', 'username', 'first_name', 'last_name', 'birth_date']
+tweet_properties = ['id', 'text', 'date']
 
 def connect_db(db_name):
     return sqlite3.connect(db_name)
 
-
 @app.before_request
 def before_request():
     g.db = connect_db(app.config['DATABASE'])
+
+def format_tweets(tweet):
+    tweet_resource = dict(zip(tweet_properties, tweet))
+    tweet_resource['uri'] = '/tweet/{}'.format(tweet['id'])
+    return tweet_resource
 
 
 # implement your views here
@@ -44,7 +48,7 @@ class Login(Resource):
 
         g.db.execute('insert into auth (user_id, access_token) values (:usr, :token);', {'usr': user_info[2], 'token': new_token})
         g.db.commit()
-
+        import pdb; pdb.set_trace()
         return {'access_token': new_token}, 201
 
 class Logout(Resource):
@@ -58,11 +62,50 @@ class Logout(Resource):
         if not logout_req['access_token']:
             return '', 401
 
-        # remove from the auth table
         g.db.execute("delete from auth where access_token = :token;", {'token': logout_req['access_token']})
         g.db.commit()
 
         return '', 204
+
+class Profile(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('first_name', type = str, help = 'First name for profile.', required = True)
+        self.reqparse.add_argument('last_name', type = str, help = 'Last name for profile.', required = True)
+        self.reqparse.add_argument('birth_date', type = str, help = 'Birthdate for profile.', required = True)
+        self.reqparse.add_argument('access_token', type = str, help = 'Access token for session.')
+
+    def get(self, username):
+        g.db.row_factory = sqlite3.Row
+        user_info = g.db.execute("select id, username, first_name, last_name, birth_date from user where username = :user;", {'user': username}).fetchone()
+
+        if not user_info:
+            return '', 404
+
+        profile = dict(zip(profile_properties, user_info))
+
+        tweets = g.db.execute("select id, content, created from tweet where user_id = :user;", {'user': profile['user_id']}).fetchall()
+
+        profile['tweet'] = [format_tweets(tweet) for tweet in tweets]
+        profile['tweet_count'] = len(tweets)
+
+        return profile
+
+    def post(self):
+        profile_req = self.reqparse.parse_args(strict =  True)
+
+        if not profile_req['access_token']:
+            return '', 401
+
+        user = g.db.execute("select user_id from auth where access_token = :token;", {'token': profile_req['access_token']}).fetchone()
+
+        if not user:
+            return '', 401
+
+        g.db.execute('update user set first_name = :first, last_name = :last, birth_date = :birth where id = :usr', {'first': profile_req['first_name'], 'last': profile_req['last_name'], 'birth': profile_req['birth_date'], 'usr': user[0]})
+        g.db.commit()
+
+        return '', 201
 
 
 @app.errorhandler(404)
@@ -76,3 +119,4 @@ def not_found(e):
 
 api.add_resource(Login, '/login')
 api.add_resource(Logout, '/logout')
+api.add_resource(Profile, '/profile/<string:username>', '/profile')
